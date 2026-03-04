4bb9a70dd87e8f120f459483d146f88412ab7928
mrazeen0705-gif <mrazeen0705@gmail.com>
2026-03-04 11:08:15 +0200

    First submission

---
+++ src/main/java/com/mycompany/tableschairs/Tableschairs.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tableschairs;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Tableschairs {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("  X                                                       X  ");
+        System.out.println("  X                        *******                        X  ");
+        System.out.println("  X                           |                           X  ");
+        System.out.println("  X                   X X X X X X X X X                   X  ");
+        System.out.println("  X                   X               X                   X  ");
+        System.out.println("  X X X X X           X               X           X X X X X  ");
+        System.out.println("  X       X           X     JAVA      X           X       X  ");
+        System.out.println("  X       X           X               X           X       X  ");
+        System.out.println("  X       X           X               X           X       X  ");
+        System.out.println(" ------------------------------------------------------------");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
